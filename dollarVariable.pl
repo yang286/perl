0bccd88f483eb7fdc0b275f3877c57c23794a784
Royyang <dsh808@126.com>
2017-05-19 02:37:39 +0800

    write some examples from the book

--- dollarVariable.pl
+++ dollarVariable.pl
@@ -3,8 +3,6 @@ foreach $a (1..10) {
 	print "I can count to $a\n";
 		foreach $b (1..10) {
 			print "I can count to $b\n";
-				foreach $c (1..10) {
-					print "I can count to $c\n";
-				}
+				
 		}
 }
